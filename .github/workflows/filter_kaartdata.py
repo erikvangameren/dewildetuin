1e419ff4d03b4cf90303d26139516040b756d365
Erik van Gameren <erixcc@gmail.com>
2022-07-26 11:45:02 +0200

    Create filter_kaartdata.py

---
+++ .github/workflows/filter_kaartdata.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+url1 = 'https://www.nrc.nl/feeds/wilde-tuin/kaart.csv'
+df = pd.read_csv(url1)
+df.loc[(df['soortgroep'] != 'Planten') & (df['soortgroep'] != 'Vogels')]
+df = df[['datum','soort','soortgroep','foto']]
+print(df.head(10))
+df.to_csv(r'beestjesselectie.csv', index = True)
