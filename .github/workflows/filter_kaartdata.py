3ca43daf19452b8cd3a2abb8c4a55d517163473e
Erik van Gameren <erixcc@gmail.com>
2022-08-02 15:40:09 +0200

    Update filter_kaartdata.py

--- .github/workflows/filter_kaartdata.py
+++ .github/workflows/filter_kaartdata.py
@@ -1,9 +1,8 @@
 import pandas as pd
-import numpy as np
 
-url1 = 'https://www.nrc.nl/feeds/wilde-tuin/kaart.csv'
-df = pd.read_csv(url1)
-df.loc[(df['soortgroep'] != 'Planten') & (df['soortgroep'] != 'Vogels')]
-df = df[['datum','soort','soortgroep','foto']]
+url1='https://www.nrc.nl/feeds/wilde-tuin/kaart.csv'
+df=pd.read_csv(url1)
+df.loc[(df['soortgroep']!='Planten') & (df['soortgroep']!='Vogels')]
+df=df[['datum','soort','soortgroep','foto']]
 print(df.head(10))
-df.to_csv(r'beestjesselectie.csv', index = True)
+df.to_csv(r'beestjesselectie.csv',index=True)
